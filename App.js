b0b5e96814dda9e17638cb1791da08c2864c62e0
Jeevan s <aryajeevan68@gmail.com>
2025-02-13 21:20:34 +0530

    updated

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import Counter from "./Counter";
 import Nav from "./Nav";
+import Paggi from "./Paggi";
 function App() {
   return (
     <div >
     <Nav/>
     <Counter/>
+    <Paggi/>
     </div>
 
 
