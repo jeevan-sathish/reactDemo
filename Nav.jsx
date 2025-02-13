3d338550e147a71974bdbc4cafab2b27f004e7f1
Jeevan s <aryajeevan68@gmail.com>
2025-02-13 17:33:31 +0530

    updated nav

--- Nav.jsx
+++ Nav.jsx
@@ -4,6 +4,12 @@ const Nav = () => {
   return (
     <div>
         <h1>this is nav</h1>
+        <ul>
+            <l1>1</l1>
+            <l1>2</l1>
+            <l1>3</l1>
+            <l1>4</l1>
+        </ul>
     </div>
   )
 }
